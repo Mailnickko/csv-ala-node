5c06eba9c269ad46a6bfc4a99f9f62ae4ed8f532
Nicholas Ko <mailnickko@gmail.com>
2018-08-29 02:43:47 -0700

    add read stream for csv and log contents

--- src/normalize.js
+++ src/normalize.js
@@ -1,2 +1,14 @@
 const fs = require('fs');
 const parser = require('csv-parse');
+
+const csvFile = 'sample.csv';
+
+const readStream = fs.createReadStream(csvFile, 'utf8').pipe(
+  parser({
+    deliminator: ','
+  })
+);
+
+readStream.on('data', chunk => {
+  console.log(chunk);
+});
