860b00cd550421ab9297740bd3deeb7a17d72f71
Nicholas Ko <mailnickko@gmail.com>
2018-08-29 18:15:36 -0700

    add logic for output file

--- src/normalize.js
+++ src/normalize.js
@@ -3,36 +3,61 @@ const parser = require('csv-parse');
 const { formatTimestamp, formatZipcode, formatName, formatDuration, combineDurations } = require('./config/utils');
 
 const csvFile = 'sample.csv';
+console.log('__', __dirname);
+const outputPath = `${__dirname}/../output/`;
 
-const readStream = fs.createReadStream(csvFile, 'utf8').pipe(
-  parser({
-    deliminator: ','
-  })
-);
+// Check to see if output directory exists, if not make it!
+/**
+ * @function makeOutputDir
+ * @param {string} dirPath - the path to make output directory
+ * @returns {void} returns nothing
+ */
+const makeOutputDir = dirPath => {
+  if (!fs.existsSync(dirPath)) {
+    fs.mkdirSync(dirPath);
+  }
+};
 
-let isHeaderLine = true;
+/**
+ * @function createCSVOutput
+ * @returns {void} returns nothing
+ * @description
+ * Parse through csvFile, make transformations, then output to destinationFile
+ */
+const createCSVOutput = () => {
+  const destinationFile = csvFile.split('.').join('-normalized');
+  const writeStream = fs.createWriteStream(outputPath + destinationFile);
+  const readStream = fs.createReadStream(csvFile, 'utf8').pipe(
+    parser({
+      deliminator: ','
+    })
+  );
+  let isHeaderLine = true;
+  readStream
+    .on('data', chunk => {
+      let formatted;
+      if (isHeaderLine) {
+        isHeaderLine = false;
+        formatted = chunk;
+      } else {
+        const [timestamp, address, zipcode, fullName, fooDur, barDur, totalDur, notes] = chunk;
+        formatted = [
+          formatTimestamp(timestamp),
+          address,
+          formatZipcode(zipcode),
+          formatName(fullName),
+          formatDuration(fooDur),
+          formatDuration(barDur),
+          combineDurations(formatDuration(fooDur), formatDuration(barDur)),
+          notes
+        ].join(',');
+      }
+      writeStream.write(formatted + '\n');
+    })
+    .on('end', () => {
+      console.log('We done');
+    });
+};
 
-readStream
-  .on('data', chunk => {
-    let formatted;
-    if (isHeaderLine) {
-      isHeaderLine = false;
-      formatted = chunk;
-    } else {
-      const [timestamp, address, zipcode, fullName, fooDur, barDur, totalDur, notes] = chunk;
-      formatted = [
-        formatTimestamp(timestamp),
-        address,
-        formatZipcode(zipcode),
-        formatName(fullName),
-        formatDuration(fooDur),
-        formatDuration(barDur),
-        combineDurations(formatDuration(fooDur), formatDuration(barDur)),
-        notes
-      ];
-    }
-    console.log(formatted);
-  })
-  .on('end', () => {
-    console.log('We done');
-  });
+makeOutputDir(outputPath);
+createCSVOutput();
